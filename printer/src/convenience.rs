849fe1eb35e40e931bb054c10a12c0e9136b2486
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2023-03-26 13:25:41 +0200

    revert accidentally committed code

--- printer/src/convenience.rs
+++ printer/src/convenience.rs
@@ -116,15 +116,4 @@ impl Printer {
             ..BreakToken::default()
         });
     }
-
-    #[inline]
-    pub fn enclose<S, F>(&mut self, before: S, after: S, block: F)
-    where
-        S: Into<Cow<'static, str>>,
-        F: FnOnce(),
-    {
-        self.word(before);
-        block();
-        self.word(after);
-    }
 }
