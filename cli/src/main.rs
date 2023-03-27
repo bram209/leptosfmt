9b1473d4ccd8ef2bdec0e599d6d0086a108d1335
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2023-03-27 10:29:38 +0200

    fix default brace style in cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -31,7 +31,7 @@ fn main() {
     let settings = FormatterSettings {
         max_width: args.max_width,
         tab_spaces: args.tab_spaces,
-        attr_value_brace_style: AttributeValueBraceStyle::AlwaysUnlessLit,
+        attr_value_brace_style: AttributeValueBraceStyle::WhenRequired,
     };
 
     let is_dir = fs::metadata(&args.input_pattern)
