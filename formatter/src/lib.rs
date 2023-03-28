74c651c078d77739c32f0bd1f287e8f4c402f128
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2023-03-28 15:02:11 +0200

    chore: make macro collecting functionality public

--- formatter/src/lib.rs
+++ formatter/src/lib.rs
@@ -9,6 +9,7 @@ mod source_file;
 #[cfg(test)]
 mod test_helpers;
 
+pub use collect::collect_macros_in_file;
 pub use formatter::*;
 
 pub fn format_file(path: &Path, settings: FormatterSettings) -> Result<String, FormatError> {
